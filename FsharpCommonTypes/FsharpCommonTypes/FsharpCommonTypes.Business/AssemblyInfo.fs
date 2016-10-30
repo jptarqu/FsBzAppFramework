5b565523a4be206ee49058047a2d3881ac75789b
jtarquino <jtarquino@5GMZTY1>
2016-10-29 23:18:43 -0400

    common bz

--- FsharpCommonTypes/FsharpCommonTypes/FsharpCommonTypes.Business/AssemblyInfo.fs
+++ FsharpCommonTypes/FsharpCommonTypes/FsharpCommonTypes.Business/AssemblyInfo.fs
@@ -1,6 +1,5 @@
-﻿namespace FsCommonTypes.Core.AssemblyInfo
+﻿namespace FsharpCommonTypes.Business.AssemblyInfo
 
-open System.Resources
 open System.Reflection
 open System.Runtime.CompilerServices
 open System.Runtime.InteropServices
@@ -8,15 +7,22 @@ open System.Runtime.InteropServices
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[<assembly: AssemblyTitle("FsCommonTypes.Core")>]
+[<assembly: AssemblyTitle("FsharpCommonTypes.Business")>]
 [<assembly: AssemblyDescription("")>]
 [<assembly: AssemblyConfiguration("")>]
 [<assembly: AssemblyCompany("")>]
-[<assembly: AssemblyProduct("FsCommonTypes.Core")>]
+[<assembly: AssemblyProduct("FsharpCommonTypes.Business")>]
 [<assembly: AssemblyCopyright("Copyright ©  2016")>]
 [<assembly: AssemblyTrademark("")>]
 [<assembly: AssemblyCulture("")>]
-[<assembly: NeutralResourcesLanguage("en")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("92f08a01-e4cf-4644-8981-7450b1b0fa0d")>]
 
 // Version information for an assembly consists of the following four values:
 // 
@@ -28,8 +34,8 @@ open System.Runtime.InteropServices
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [<assembly: AssemblyVersion("1.0.*")>]
-[<assembly: AssemblyVersion("1.0.7.0")>]
-[<assembly: AssemblyFileVersion("1.0.7.0")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
 
 do
     ()
---
+++ FsharpCommonTypes/FsharpCommonTypes/FsharpCommonTypes.Business/Queries.fs
@@ -0,0 +1,33 @@
+﻿namespace FsharpCommonTypes.Business
+
+module Queries =
+    open Chessie.ErrorHandling
+
+    let GetFromDataPull (dataPull:unit->Async<'RowType list>) () =
+         asyncTrial {
+                let! rows = dataPull()
+                return rows |> List.toSeq
+            }
+
+module Commands =
+    open Chessie.ErrorHandling
+    open CommonValidations
+
+//    let RunValidateFuncs (input:'A) (funcs:('A->PropertyError) seq) =
+//        let result = input
+//                        |> validateAcctNumber 
+//                        |> bind validateAcctType
+//                        |> bind validateTransactionID
+//                        |> failOnWarnings 
+//        match result with 
+//        | Fail errs -> fail (ValidationErrors errs)
+//        | Pass _ -> pass input
+
+//    let UpdateAsync otherProcessing (dataSave:'Command->Async<int option>) (record:'Command) =
+//        asyncTrial {
+//                
+//            let! validatedCmd = RunValidateFuncs record
+//            let! idReturned = dataSave validatedCmd
+//            otherProcessing()
+//            return finalId
+//            }
