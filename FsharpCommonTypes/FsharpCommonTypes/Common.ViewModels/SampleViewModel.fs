a8c6e3bf0f53fdb80b337876057721e017d269e7
jtarquino <jtarquino@5GMZTY1>
2016-10-29 21:53:11 -0400

    ViewModel cleanup

--- FsharpCommonTypes/FsharpCommonTypes/Common.ViewModels/SampleViewModel.fs
+++ FsharpCommonTypes/FsharpCommonTypes/Common.ViewModels/SampleViewModel.fs
@@ -50,14 +50,69 @@ type SampleDocList =
             member this.GetValidationErrors () = Seq.empty
             
 type SampleEditDocViewModel(dialogService:IDialogService, screenManager:ScreenManager) =
+//TODo allow passing of existing model to this, well, just like LS
     let screenName = "Sales Person" 
     let screenId = CommandScreen.GenerateId screenName
-    let afterSuccess doc cmdResult =
-        screenManager.RemoveScreen screenId
-        ()
-            
-    let afterFailure doc cmdResult =
-        ()
+    let createModel () =
+        let now = System.DateTime.Now
+        let model ={SampleDoc.Name= BusinessTypes.LongName "Alabama" ; SalesRegion = BusinessTypes.IdNumber 1; SalesDate =  BusinessTypes.PastDateTime now; QuantityId = BusinessTypes.IdNumber -1} 
+        model
+    let createModelAsync () =
+        async {
+            return createModel()
+        }
+    let doNothingCmd  doc =
+        async {
+            return { CommandResult.Errors = Seq.empty; CommandResult.Message = "Thanks!" }
+        }
+    let onSaved doc =
+        async {
+            let! resp =  (dialogService.PromptMessage screenId "Test" "Test")
+                        |> Async.AwaitIAsyncResult 
+            screenManager.RemoveScreen screenId
+            return { CommandResult.Errors = Seq.empty; CommandResult.Message = "Thanks!" }
+        }
+    let addCommands (baseDocVm:DocViewModelBase<SampleDocList>) docGetter =
+        let afterSuccess doc cmdResult =
+            ()
+        let afterFailure cmdResult =
+            ()
+        let cmdEditDef = { CommandDefinition.CmdName = "Save"; CommandDefinition.CmdExecuter = onSaved; CanRunCheck = BusinessTypes.IsValidModel }
+        let cancelCmdDef = CommandDefinition.CancelCmdDefinition
+        let cmdEdit = CommandViewModel(cmdEditDef, afterSuccess, afterFailure,  docGetter)
+        let cancelCmd = CommandViewModel(cancelCmdDef, afterSuccess, afterFailure,  docGetter)
+        baseDocVm.PrimaryCommands.Add cmdEdit
+        baseDocVm.SecondaryCommands.Add cancelCmd
+    let addChildViews (baseDoc:DocViewModelBase<SampleDocList>) docGetter =
+        let simpleChoices (doc:SampleDoc) = 
+            let newRand = System.Random().Next(1,6).ToString()
+            [ {ResultId= 1; ResultLabel= "Test 1";  };
+            {ResultId= 2; ResultLabel= "Test " ;  } ;
+            {ResultId= 3; ResultLabel= "Test 3 " + doc.Name.ToString();  }  ]
+        let cmd = { CommandDefinition.CmdName = "Save"; CommandDefinition.CmdExecuter = DoNothingCmd; CanRunCheck = BusinessTypes.IsValidModel }
+        let cancelCmd = CommandDefinition.CancelCmdDefinition
+        let afterCancel = afterSuccess
+        let doc = DocViewModel(model, cmd, afterSuccess,cancelCmd, afterCancel, Seq.empty)
+        SingleInputViewModel.AddTextInputViewModel doc (doc.GetRootView()) SampleDoc.DefinitionName 
+        SingleInputViewModel.AddMaskedTextInputViewModel doc (doc.GetRootView()) SampleDoc.DefinitionQuantityId "999999990"
+        SingleInputViewModel.AddDateInputViewModel doc (doc.GetRootView()) SampleDoc.DefinitionSalesDate 
+        SimpleChoicesViewModel.AddSimpleChoicesViewModel doc (doc.GetRootView()) SampleDoc.DefinitionSalesRegion simpleChoices 
+        
+    // TODO the rest of the class seems repeatable... try DRYing
+    let intialDoc = createModel()
+    let mutable baseDocViewModel = new DocViewModelBase<SampleDoc>(intialDoc)
+    let docGetter () = baseDocViewModel.GetCurrentDoc
+   
+    member this.Init () = 
+        async {
+            let! initialDoc = createModelAsync() //async in case op takes time (like fetching from server)
+            addChildViews baseDocViewModel docGetter
+            addCommands baseDocViewModel docGetter
+            baseDocViewModel.ReloadDoc initialDoc //maybe this makes more sense than Init
+            let screen = CommandScreen(baseDocViewModel, screenName, screenId)
+            screenManager.AddScreen screen // all we need to disaplay because the viewModel has been setup already and ready to go
+        }
+
 type SampleDocListViewModel(dialogService:IDialogService, screenManager:ScreenManager) =
     let screenName = "List Sales People" 
     let screenId = CommandScreen.GenerateId screenName
@@ -184,18 +239,7 @@ module Sample =
         screenManager.AddScreen screen // TODO should this be here or bootstrapper??
 //        (screen :> IScreen).Init()
         screen
-        
-    let CreateSampleListDoc () =
-        let now =  Some System.DateTime.Now
-        { 
-            SampleDocList.Docs =
-                [
-                    {Name=  "Alabama" ; SalesRegion =   1; SalesDate =    now} 
-                    {Name=  "Alabama" ; SalesRegion =  2; SalesDate =    now} 
-                    {Name=  "Colorado" ; SalesRegion =  3; SalesDate =    now} 
-                    {Name=  "California" ; SalesRegion = 4; SalesDate =    now} 
-                ]
-            SelectedItem = None
-        }
+     
+
         
   
